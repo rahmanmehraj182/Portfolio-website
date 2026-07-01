4b5312c249de2f1a67b6259d9ca5f9defd97f64c
tanvirhasanturjoy <tanvirhasanturjoy72@gmail.com>
2026-07-01 15:09:10 +0600

    added

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 
-const express = require("express");
-const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
